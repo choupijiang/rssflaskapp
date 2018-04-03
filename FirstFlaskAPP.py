c0c9f6dc61546c8422a71fe653c37d5470575617
张生虎 <zhangshenghu@zhangshenghudeMacBook-Pro.local>
2018-04-03 18:55:40 +0800

    add weather

--- FirstFlaskAPP.py
+++ FirstFlaskAPP.py
@@ -2,7 +2,8 @@ from flask import Flask
 import feedparser
 from flask import render_template
 from flask import request
-
+import urllib
+import json
 
 app = Flask(__name__)
 
@@ -10,61 +11,6 @@ RSS_FEEDS = {"people": "http://www.people.com.cn/rss/politics.xml",
              "finance": "http://www.people.com.cn/rss/finance.xml",
              }
 
-
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello World!'
-
-# @app.route('/')
-# @app.route('/people')
-# def get_people():
-#     return get_news("people")
-#
-# @app.route("/finance")
-# def get_finance():
-#     return get_news("finance")
-
-
-# @app.route("/")
-# @app.route("/<cat>")
-# def get_news(cat="people"):
-#     feed = feedparser.parse(RSS_FEEDS[cat])
-#     first_artical = feed["entries"][0]
-#     return """<html>
-#         <body>
-#             <h1>pepple</h1>
-#             <b>{0}</b><br>
-#             <i>{1}</i><br>
-#             <p>{2}</p><br>
-#         </body>
-#     </html>
-#     """.format(first_artical.get("title"), first_artical.get("published"), first_artical.get("summary"))
-
-# @app.route("/")
-# @app.route("/<cat>")
-# def get_news(cat="people"):
-#     feed = feedparser.parse(RSS_FEEDS[cat])
-#     first_artical = feed["entries"][0]
-#     return render_template("home.html",
-#                 title=first_artical.get("title"),
-#                 published=first_artical.get("published"),
-#                 summary=first_artical.get("summary")
-#             )
-
-# @app.route("/")
-# @app.route("/<cat>")
-# def get_news(cat="people"):
-#     feed = feedparser.parse(RSS_FEEDS[cat])
-#     first_artical = feed["entries"][0]
-#     return render_template("home.html", article=first_artical)
-   
-# @app.route("/")
-# @app.route("/<string:cat>/")
-# def get_news(cat="people"):
-#     feed = feedparser.parse(RSS_FEEDS[cat])
-#     articles = feed["entries"]
-#     return render_template("home.html", articles=articles)
 
 
 @app.route("/")
@@ -75,8 +21,20 @@ def get_news():
     else:
         cat = query.lower()
     feed = feedparser.parse(RSS_FEEDS[cat])
-    return render_template("home.html", articles=feed["entries"])
+    weather = get_weather("北京")
+    return render_template("home.html", weather=weather, articles=feed["entries"])
         
+
+def get_weather(city):
+    api_url = "https://www.sojson.com/open/api/weather/json.shtml?city={}".format(city)
+    print(api_url)
+    url = urllib.parse.quote(api_url)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed["data"]["quality"], "temperature": parsed["data"]["wendu"], "city": parsed["city"]}
+    return weather
 
 if __name__ == '__main__':
     app.run(port=8088, debug=True)
