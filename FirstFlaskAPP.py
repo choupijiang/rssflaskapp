d735ce428c6ddc18cd67451da9647fc3369e94c4
张生虎 <zhangshenghu@zhangshenghudeMacBook-Pro.local>
2018-04-03 23:41:55 +0800

    add some new

--- FirstFlaskAPP.py
+++ FirstFlaskAPP.py
@@ -4,6 +4,9 @@ from flask import render_template
 from flask import request
 import urllib
 import json
+import string
+import ssl
+
 
 app = Flask(__name__)
 
@@ -14,25 +17,38 @@ RSS_FEEDS = {"people": "http://www.people.com.cn/rss/politics.xml",
 
 
 @app.route("/")
-def get_news():
-    query = request.args.get("cat")
+def home():
+    cat = request.args.get("cat")
+    if not cat:
+        cat = "people"
+    articles = get_news(cat)
+
+    context=urllib.request.urlopen( 'http://pv.sohu.com/cityjson').read()
+    city=context.decode("gb2312").split("=")[1].split(",")[2].split('"')[3]
+    if not city:
+
+        city = "北京"
+    weather = get_weather(city)
+    return render_template("home.html", articles=articles, weather=weather)
+
+
+def get_news(query):
     if not query or query.lower() not in RSS_FEEDS:
         cat = "people"
     else:
         cat = query.lower()
     feed = feedparser.parse(RSS_FEEDS[cat])
-    weather = get_weather("北京")
-    return render_template("home.html", weather=weather, articles=feed["entries"])
+    return feed["entries"]
         
 
 def get_weather(city):
     api_url = "https://www.sojson.com/open/api/weather/json.shtml?city={}".format(city)
-    print(api_url)
-    url = urllib.parse.quote(api_url)
-    data = urllib.request.urlopen(url).read()
-    parsed = json.loads(data)
+    parsed_url = urllib.parse.quote(api_url, safe=string.printable)
+    context = ssl._create_unverified_context()
+    data = urllib.request.urlopen(parsed_url, context=context)
+    parsed = json.loads(data.read())
     weather = None
-    if parsed.get("weather"):
+    if parsed.get("data"):
         weather = {"description": parsed["data"]["quality"], "temperature": parsed["data"]["wendu"], "city": parsed["city"]}
     return weather
 
